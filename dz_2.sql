fc6b73a4b117c6911e2c0b155898b6c2b611cda8
Evgeniy Veselskiy <jeka.vesel@mail.ru>
2022-07-28 09:30:33 +0600

    finish

---
+++ dz_2.sql
@@ -0,0 +1,58 @@
+CREATE TABLE IF NOT EXISTS musical_genre 
+(
+	id SERIAL primary key,
+	name VARCHAR(20) not null
+);
+
+CREATE TABLE IF NOT EXISTS singers
+(
+	id SERIAL primary key,
+	name VARCHAR(20) not NULL,
+	musical_genre VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS singers_musical_genre
+(
+	id SERIAL PRIMARY KEY,
+	musical_genre_id INT REFERENCES musical_genre(id),
+	singers_id INT REFERENCES singers(id),
+	CONSTRAINT pk PRIMARY KEY (musical_genre_id, singers_id)
+);
+
+CREATE TABLE IF NOT EXISTS compilation
+(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
+	year INT NOT NULL CHECK (YEAR>0),
+	tracks VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tracks
+(
+	id SERIAL primary key,
+	name VARCHAR(20) NOT NULL,
+	duration VARCHAR(20) NOT NULL,
+	album INT NOT NULL REFERENCES albums(id)
+);
+
+CREATE TABLE IF NOT EXISTS compilation_tracks
+(
+	id SERIAL primary key,
+	compilation_id INT REFERENCES compilation(id),
+	tracks_id INT REFERENCES singers(id)
+);
+
+CREATE TABLE IF NOT EXISTS albums
+(
+	id SERIAL primary key,
+	name VARCHAR(20) NOT NULL,
+	year INT NOT NULL CHECK (YEAR > 0),
+	singer VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS singers_albums
+(
+	id SERIAL PRIMARY KEY,
+	albums_id INT REFERENCES albums(id),
+	singers_id INT REFERENCES singers(id)
+);
